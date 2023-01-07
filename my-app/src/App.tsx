837f6db6c709777dbd6dfd860bb8cc0c56179a81
swathi <swathysidharthan@gmail.com>
2023-01-07 10:47:51 +0100

    fetch data from api

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+interface userInfo {
+  name: string,
+  age: number,
+  address: string
+}
+
 function App() {
+  const [userData, setUserData] = useState<userInfo>({
+    name: '',
+    age: 0,
+    address: '',
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+
+      const firstUser = data.results[0];
+
+      setUserData(prev => {
+        return {
+          ...prev,
+          name: firstUser.name.first,
+          age: firstUser.dob.age,
+          address: firstUser.location.street.name,
+        }
+      });
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      
+      <p>{userData.name}</p>
+      <p>{userData.age}</p>
+      <p>{userData.address}</p>
     </div>
   );
 }
